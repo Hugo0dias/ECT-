0be7f114c72a86af17886f4b455f56023e8608e8
Hugo0dias <hugomdias@ua.pt>
2023-05-16 19:47:19 +0100

    Ex1 Aula02

---
+++ POO/src/aula02/MileToKm.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class MileToKm {
+
+    public static void main(String[] args) {
+
+        double km;
+        double miles;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kilometros: ");
+        km = input.nextDouble();
+
+        miles = km / 1.609;
+
+        System.out.printf("%.0f kilometros é equivalente a %.5f milhas", km, miles);
+
+        input.close();
+    }
+
+}
